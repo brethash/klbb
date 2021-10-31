2955736efd71809b4c548a3e24527055e1e99e38
Bret Hash <bret@brethash.com>
2021-10-30 23:33:37 -0600

    We're Typescripting now

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-'use strict';
-
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-
-    entry: './src/index.js',
-
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
-    },
-
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
+  entry: './src/game.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'ts-loader'
+      },
+      {
+        test: require.resolve('Phaser'),
+        loader: 'expose-loader',
+        options: { exposes: { globalName: 'Phaser', override: true } }
+      }
     ]
-
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+    publicPath: '/dist/',
+    host: 'localhost',
+    port: 8080,
+    open: false
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 };
