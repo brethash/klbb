e7864a14471ebe158ea302dc7b9537b6f3bbbf84
brethash <bret@brethash.com>
2021-11-02 17:26:44 -0600

    Upleveling our object modeling game with a Character class, jump sound, and hit points (#21)

--- src/objects/character.ts
+++ src/objects/character.ts
@@ -1,24 +1,58 @@
-import { IImageConstructor } from '../interfaces/image.interface';
+import { CharacterConstructor } from '../interfaces/character.interface';
+import Phaser from "phaser";
 
-export class Character extends Phaser.GameObjects.Image {
+export class Character extends Phaser.Physics.Arcade.Sprite {
   body: Phaser.Physics.Arcade.Body;
+  private _hitPoints: number | boolean = false;
+  private _jumpSound: string;
 
-  constructor(aParams: IImageConstructor) {
-    super(aParams.scene, aParams.x, aParams.y, aParams.texture, aParams.frame);
+  constructor(aParams: CharacterConstructor) {
+    super(aParams.scene,
+        aParams.startX,
+        aParams.startY,
+        aParams.spriteId);
 
     this.initSprite();
     this.initPhysics();
+    if (aParams.jumpSound) {
+      this.jumpSound = aParams.jumpSound;
+    }
+    if (aParams.hitPoints) {
+      this.hitPoints = aParams.hitPoints;
+    }
+
     this.scene.add.existing(this);
   }
 
   private initSprite() {
-    this.setScale(0.5);
+    // todo: this is lol
+    this.setScale(1.0);
   }
 
   private initPhysics() {
+    // todo: take the rest of the stuff like physics and assign them here
     this.scene.physics.world.enable(this);
-    this.body.setVelocity(100, 200);
-    this.body.setBounce(1, 1);
-    this.body.setCollideWorldBounds(true);
+    // this.body.setVelocity(100, 200);
+    // this.body.setBounce(1, 1);
+    // this.body.setCollideWorldBounds(true);
   }
+
+    get hitPoints(): number | boolean {
+        return this._hitPoints;
+    }
+
+    set hitPoints(value: number | boolean) {
+        this._hitPoints = value;
+    }
+    get jumpSound(): string {
+        return this._jumpSound;
+    }
+
+    set jumpSound(value: string) {
+        this._jumpSound = value;
+    }
 }
+
+// //  Player physics properties. Give the little guy a slight bounce.
+// this.player.setBounce(0.2);
+// this.player.setCollideWorldBounds(true);
